8ca8764f7412360138f2c40378a45b713ad67629
Rodrigues <anjosantonio011@gmail.com>
2025-05-11 12:02:56 -0300

    Implementa Google Tag Manager no layout principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,28 +24,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <head />
-      <body className={`${inter.variable} font-sans`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N5TKRD6W"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-        
+      <head>
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script id="google-tag-manager" strategy="beforeInteractive" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N5TKRD6W');`}
-        </Script>
+          })(window,document,'script','dataLayer','GTM-N5TKRD6W');`
+        }} />
         {/* End Google Tag Manager */}
+      </head>
+      <body className={`${inter.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5TKRD6W"
+        height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         
         <ThemeProvider
           attribute="class"
